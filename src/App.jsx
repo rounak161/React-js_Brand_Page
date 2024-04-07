989cfa3dde9c335fd8fe9e6313d0f244831bd240
Rounak Kamble <rounakr@iitbhilai.ac.in>
2024-04-07 15:41:11 +0530

    initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navigation from "./components/Navigation";
+import HeroSection from "./components/Hero";
+import "./App.css";
+const App = () => {
+  return (
+    <div>
+      <Navigation />
+      <HeroSection />
+    </div>
+  );
+};
+
+export default App;
